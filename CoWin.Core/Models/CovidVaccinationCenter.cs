3a0a112f2685e6b41bd3d4b2df0d44062236de3e
Saurav Saha <contact.srvsaha@gmail.com>
2021-05-30 20:08:08 +0530

    Update CovidVaccinationCenter.cs

--- CoWin.Core/Models/CovidVaccinationCenter.cs
+++ CoWin.Core/Models/CovidVaccinationCenter.cs
@@ -42,6 +42,7 @@ namespace CoWiN.Models
             }
             else if (response.StatusCode == HttpStatusCode.Forbidden || response.StatusCode == HttpStatusCode.TooManyRequests)
             {
+                isIPThrottled = false;
                 new Thread(new ThreadStart(IPThrolledNotifier)).Start();
                 Console.ForegroundColor = ConsoleColor.DarkRed;
                 Console.WriteLine($"[FATAL] Response From Server: Too many hits from your IP address, hence request has been blocked. You can try following options :\n1.(By Default) Wait for {_configuration["CoWinAPI:ThrottlingRefreshTimeInSeconds"]} seconds, the Application will Automatically resume working.\n2.Switch to a different network which will change your current IP address.\n3.Close the application and try again after sometime");
@@ -75,6 +76,7 @@ namespace CoWiN.Models
             }
             else if (response.StatusCode == HttpStatusCode.Forbidden || response.StatusCode == HttpStatusCode.TooManyRequests)
             {
+                isIPThrottled = false;
                 new Thread(new ThreadStart(IPThrolledNotifier)).Start();
                 Console.ForegroundColor = ConsoleColor.DarkRed;
                 Console.WriteLine($"[FATAL] Response From Server: Too many hits from your IP address, hence request has been blocked. You can try following options :\n1.(By Default) Wait for {_configuration["CoWinAPI:ThrottlingRefreshTimeInSeconds"]} seconds, the Application will Automatically resume working.\n2.Switch to a different network which will change your current IP address.\n3.Close the application and try again after sometime");
@@ -351,6 +353,7 @@ namespace CoWiN.Models
             }
             else if (response.StatusCode == HttpStatusCode.Forbidden || response.StatusCode == HttpStatusCode.TooManyRequests)
             {
+                isIPThrottled = false;
                 new Thread(new ThreadStart(IPThrolledNotifier)).Start();
                 Console.ForegroundColor = ConsoleColor.DarkRed;
                 Console.WriteLine($"[FATAL] Response From Server: Too many hits from your IP address, hence request has been blocked. You can try following options :\n1.(By Default) Wait for {_configuration["CoWinAPI:ThrottlingRefreshTimeInSeconds"]} seconds, the Application will Automatically resume working.\n2.Switch to a different network which will change your current IP address.\n3.Close the application and try again after sometime");
